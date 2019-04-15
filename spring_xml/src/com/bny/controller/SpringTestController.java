8e626687e8e8e10e249242e3909f6689c5e77db0
namyulbae <bny64@naver.com>
2019-04-15 17:48:20 +0900

    none

---
+++ spring_xml/src/com/bny/controller/SpringTestController.java
@@ -0,0 +1,18 @@
+package com.bny.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringTestController {
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public ModelAndView TestController(ModelAndView mnv){
+		System.out.println("index");				
+		mnv.addObject("message", "hello~ Spring World");
+		mnv.setViewName("index");
+		return mnv;
+	}
+}
