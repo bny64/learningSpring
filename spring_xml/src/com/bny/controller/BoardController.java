fea95b547f07777a1ef4f715173b0512b9d14599
namyulbae <bny64@naver.com>
2019-05-21 11:35:27 +0900

    boardController

---
+++ spring_xml/src/com/bny/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.bny.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BoardController {
+
+	private static Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+}
