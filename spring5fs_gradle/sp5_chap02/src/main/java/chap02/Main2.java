ea792d01831d8dd3dcfa41aa2a1dad894cc63691
bny64 <bny64@naver.com>
2019-06-12 22:35:21 +0900

    a

--- spring5fs_gradle/sp5_chap02/src/main/java/chap02/Main2.java
+++ spring5fs_gradle/sp5_chap02/src/main/java/chap02/Main2.java
@@ -1,5 +1,23 @@
 package chap02;
 
-public class Main2 {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main2 {
+	
+	private Greeter g3;
+	
+	//생성자를 통한 주입.
+	public void setMethod(Greeter g) {
+		this.g3 = g;
+	}
+	
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+		System.out.println("(g1 == g2) = " + (g1 == g2));
+		ctx.close();
+		//결과값은 true. 같은 객체를 리턴하는 것을 의미한다.
+	}
 }
